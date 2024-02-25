7d75cb2b9a18599d8d05b00524c254209ec4c8f8
diclebahceli <diclebahceli24@gmail.com>
2024-02-25 15:31:23 +0300

    Feat: Implement generic write repository

--- Core/movie.Application/Interfaces/Repositories/IWriteRepository.cs
+++ Core/movie.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace movie.Application;
+﻿using movie.Domain;
 
-public interface IWriteRepository
+namespace movie.Application;
+
+public interface IWriteRepository<T> where T : class, IEntityBase, new()
 {
+
+    Task AddAsync(T entity);
+    Task AddRangeAsync(IList<T> entities);
+    Task<T> UpdateAsync(T entity);
+    Task SoftDeleteAsync(T entity);
+    Task HardDeleteAsync(T entity);
 
 }
--- Infrastructure/movie.Persistence/Repositories/WriteRepository.cs
+++ Infrastructure/movie.Persistence/Repositories/WriteRepository.cs
@@ -1,6 +1,43 @@
-﻿namespace movie.Persistence;
+﻿using Microsoft.EntityFrameworkCore;
+using movie.Application;
+using movie.Domain;
 
-public class WriteRepository
+namespace movie.Persistence;
+
+public class WriteRepository<T> : IWriteRepository<T> where T : class, IEntityBase, new()
 {
 
+    private DbContext dbContext;
+    private DbSet<T> Table => dbContext.Set<T>();
+
+    public WriteRepository(DbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+    public async Task AddAsync(T entity)
+    {
+        await dbContext.AddAsync(entity);
+    }
+
+    public async Task AddRangeAsync(IList<T> entities)
+    {
+        await dbContext.AddRangeAsync(entities);
+    }
+
+    public async Task HardDeleteAsync(T entity)
+    {
+        await Task.Run(() => dbContext.Remove(entity));
+    }
+
+    public async Task SoftDeleteAsync(T entity)
+    {
+        (entity as EntityBase).IsDeleted = true;
+        await UpdateAsync(entity);
+    }
+
+    public async Task<T> UpdateAsync(T entity)
+    {
+        await Task.Run(() => dbContext.Update(entity));
+        return entity;
+    }
 }
